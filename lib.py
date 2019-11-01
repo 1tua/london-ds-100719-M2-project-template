1d162e83f95006723481e2c931b7351bce541088
Itua Etiobhio <Itua@email.com>
2019-11-01 12:37:37 +0000

    final_change

--- lib.py
+++ lib.py
@@ -4,4 +4,20 @@ class MongoHandler():
     pass
 
 class WeatherGetter():
-    pass
+    def __init__(self, date = None):
+       self.link = f"https://api.darksky.net/forecast/{self.key}/{self.lat},{self.long},{self.date}?exclude=currently,hourly,flags”
+       self.date = date + “T15:00:00”  #set default time to 3pm
+       self.lat = 52.52 #Coordinates for Berlin
+       self.long = 13.405 #Coordinates for Berlin
+       self.key = “” #insert key here
+       self.weather = self.getweathersummary()
+    
+     def getweathersummary(self):
+       r = requests.get(self.link)
+       response = r.json()
+       try:
+           response[‘daily’][‘data’][0][‘icon’]
+       except:
+           return response[‘daily’][‘data’][0][‘summary’]  #if there is no icon then get summary
+       else:
+           return response[‘daily’][‘data’][0][‘icon’]
